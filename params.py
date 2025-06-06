f0f06e89146d39f4f398956ceddbb443bd9a9b0a
esby <luka70@naver.com>
2025-06-06 10:48:49 -0400

    [minor] 'four_tank' system에 대한 params 수정 (25.06.06)

--- params.py
+++ params.py
@@ -7,7 +7,7 @@ def running_params():
         # 'system': 'four_tank',
         'train_agent': False, # Whether to train agents. If false, Load trained agents.
         'algo': 'DDPG', # RL algorithm
-        'nsteps_train': 1e4, # Total time steps during training
+        'nsteps_train': 1e5, # Total time steps during training
         'rollout_reps': 1, # Number of episodes for rollout data
     }
     return running_params
@@ -42,7 +42,7 @@ def env_params(system):
 
     elif system == 'four_tank':
         # Simulation parameteters
-        T = 1000  # Total simulated time (min)
+        T = 2000  # Total simulated time (min)
         nsteps = 600  # Total number of steps
         delta_t = T / nsteps  # Minutes per step
         training_seed = 1
@@ -56,21 +56,27 @@ def env_params(system):
                 if i % 60 == 0:
                     setpoint = np.random.uniform(low=0.1, high=0.5)
                 setpoints.append(setpoint)
-            SP[atarget] = setpoints
+            SP[target] = setpoints
 
         # Action, observation space and initial point
-        action_space = {'low': np.array([295]),
-                        'high': np.array([302])}
-        observation_space = {'low': np.array([0.7, 300, -0.1]),
-                             'high': np.array([1, 350, 0.1])}
-        initial_point = np.array([0.8, 330, 0.0])
+        action_space = {
+            'low': np.array([0.1, 0.1]),
+            'high': np.array([10, 10])
+        }
+
+        observation_space = {
+            'low': np.array([0, ] * 6),
+            'high': np.array([0.6] * 6)
+        }
+
+        initial_point = np.array([0.141, 0.112, 0.072, 0.42, SP['h3'][0], SP['h4'][0]])
 
         r_scale = dict(zip(targets,[1e3 for _ in targets]))
 
     else:
         raise Exception(f'{system} is not a valid system.')
 
-    # Define reward to be equal to the OCP (i.e the same as the oracle)
+    # Define reward to be equal to the OCP (i.e. the same as the oracle)
     env_params = {
         'targets': targets,
         'N': nsteps,
@@ -92,5 +98,6 @@ def env_params(system):
 
     env = make_env(env_params)
     env_params['feature_names'] = env.model.info()["states"] + [f"Error_{target}" for target in targets]
+    env_params['actions'] = env.model.info()["inputs"]
 
     return env, env_params
