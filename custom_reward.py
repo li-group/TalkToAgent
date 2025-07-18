3bbd4bdeeef168b69f24fb612d379f0f85e83770
esby <luka70@naver.com>
2025-07-18 18:18:33 -0400

    [MAJOR] Multi-stage extraction system 추가 (25.07.18)

--- custom_reward.py
+++ custom_reward.py
@@ -76,41 +76,40 @@ def four_tank_reward(self, x, u, con):
     except Exception:
         return r
 
-def four_tank_reward_decomposed(env, x, u, con):
+def multistage_extraction_reward(self, x, u, con):
     Sp_i = 0
+    cost = 0
     R = 0.1
-    costs = []
-    if not hasattr(env, 'u_prev'):
-        env.u_prev = u
+    if not hasattr(self, 'u_prev'):
+        self.u_prev = u
 
-    for k in env.env_params["SP"]:
-        i = env.model.info()["states"].index(k)
-        SP = env.SP[k]
+    for k in self.env_params["SP"]:
+        i = self.model.info()["states"].index(k)
+        SP = self.SP[k]
 
-        o_space_low = env.env_params["o_space"]["low"][i]
-        o_space_high = env.env_params["o_space"]["high"][i]
+        o_space_low = self.env_params["o_space"]["low"][i]
+        o_space_high = self.env_params["o_space"]["high"][i]
 
         x_normalized = (x[i] - o_space_low) / (o_space_high - o_space_low)
         setpoint_normalized = (SP - o_space_low) / (o_space_high - o_space_low)
 
-        r_scale = env.env_params.get("r_scale", {})
+        r_scale = self.env_params.get("r_scale", {})
 
-        cost_k = (np.sum(x_normalized - setpoint_normalized[env.t]) ** 2) * r_scale.get(k, 1)
-        costs.append(cost_k)
+        cost += (np.sum(x_normalized - setpoint_normalized[self.t]) ** 2) * r_scale.get(k, 1)
 
         Sp_i += 1
-
-    u_normalized = (u - env.env_params["a_space"]["low"]) / (
-            env.env_params["a_space"]["high"] - env.env_params["a_space"]["low"]
+    u_normalized = (u - self.env_params["a_space"]["low"]) / (
+            self.env_params["a_space"]["high"] - self.env_params["a_space"]["low"]
     )
-    u_prev_norm = (env.u_prev - env.env_params["a_space"]["low"]) / (
-            env.env_params["a_space"]["high"] - env.env_params["a_space"]["low"]
+    u_prev_norm = (self.u_prev - self.env_params["a_space"]["low"]) / (
+            self.env_params["a_space"]["high"] - self.env_params["a_space"]["low"]
     )
-    env.u_prev = u
+    self.u_prev = u
 
     # Add the control cost
-    cost_u = np.sum(R * (u_normalized - u_prev_norm) ** 2)
-    costs.append(cost_u)
-
-    rs = [-cost for cost in costs]
-    return rs
+    cost += np.sum(R * (u_normalized - u_prev_norm) ** 2)
+    r = -cost
+    try:
+        return r[0]
+    except Exception:
+        return r
--- params.py
+++ params.py
@@ -1,12 +1,13 @@
 import numpy as np
 from src.pcgym import make_env
-from custom_reward import cstr_reward, four_tank_reward
+from custom_reward import cstr_reward, four_tank_reward, multistage_extraction_reward
 
 def running_params():
     running_params = {
         # 'system': 'cstr',
-        'system': 'four_tank',
-        'train_agent': False, # Whether to train agents. If false, Load trained agents.
+        # 'system': 'four_tank',
+        'system': 'multistage_extraction',
+        'train_agent': True, # Whether to train agents. If false, Load trained agents.
         'algo': 'SAC', # RL algorithm
         # 'algo': 'DDPG',  # RL algorithm
         'nsteps_train': 1e6, # Total time steps during training
@@ -23,7 +24,6 @@ def env_params(system):
         T = 300  # Total simulated time (min)
         nsteps = 600  # Total number of steps
         delta_t = T / nsteps  # Minutes per step
-        training_seed = 1
         reward = cstr_reward
 
         # Setting setpoints
@@ -51,7 +51,6 @@ def env_params(system):
         T = 8000  # Total simulated time (min)
         nsteps = 400  # Total number of steps
         delta_t = T / nsteps  # Minutes per step
-        training_seed = 1
         reward = four_tank_reward
 
         # Setting setpoints
@@ -79,6 +78,43 @@ def env_params(system):
         initial_point = np.array([0.141, 0.112, 0.072, 0.42, 0.0, 0.0])
 
         r_scale = dict(zip(targets,[1e3 for _ in targets]))
+
+    elif system == 'multistage_extraction':
+        T = 300
+        nsteps = 300
+        delta_t = T / nsteps  # Minutes per step
+        reward = multistage_extraction_reward
+
+        # x(np.ndarray): Current state[X1, Y1, X2, Y2, X3, Y3, X4, Y4, X5, Y5]
+        # u(np.ndarray): Input[L, G]
+
+        # Setting setpoints
+        SP = {}
+        targets = ['X5', 'Y1']
+        for target in targets:
+            setpoints = []
+            for i in range(nsteps):
+                if i % 30 == 0:
+                    setpoint = np.random.uniform(low=0.1, high=0.9)
+                setpoints.append(setpoint)
+            SP[target] = setpoints
+
+        action_space = {
+            'low': np.array([5, 10]),
+            'high': np.array([500, 1000])
+        }
+
+        observation_space = {
+            'low': np.array([0] * 10 + [-1] * 2),
+            'high': np.array([1] * 10 + [1] * 2)
+        }
+
+        initial_point = np.array([0.55, 0.3, 0.45, 0.25, 0.4, 0.20, 0.35, 0.15, 0.25, 0.1, 0.0, 0.0])
+
+        r_scale = {
+            'X5': 1,
+            'Y1': 1
+        }
 
     else:
         raise Exception(f'{system} is not a valid system.')
