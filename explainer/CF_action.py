9a6b1efb8bda8323702a52dd64c73c4aee6594f5
esby <luka70@naver.com>
2025-08-04 21:48:31 -0400

    [MAJOR] CF_action done (25.08.04)

--- explainer/CF_action.py
+++ explainer/CF_action.py
@@ -12,25 +12,37 @@ def cf_by_action(t_begin, t_end, actions, values, policy, horizon=10):
     Counterfactual analysis of action to future trajectories.
     i.e.) "What would the future states would change if we execute this action at specific time step?"
           "Why does the policy made this action instead of this?"
-    - Get a rollout data of trained policy, except only for 't_begin<=t<=t_end', where we execute predefined counterfactual action
+    Get a rollout data of trained policy, except only for 't_begin<=t<=t_end', where we execute predefined counterfactual action
+    Args:
+        t_begin (Optional[int, float]): Start of the time interval to be queried
+        t_end (Optional[int, float]): End of the time interval to be queried
+        actions (list): Action variables to be intervened
+        values (list): Intervened values of each action variable
+        policy (BaseAlgorithm): Trained RL actor, using stable-baselines3
+        horizon (int): Length of future horizon to be explored
+    Returns:
+        figures (list): List of decomposed reward figures
+        evaluator.data (dict): Forward rollout data of actual and counterfactual scenarios
     """
 
+    # Assert that the queried action values are within the action space
     assert (values <= env_params['a_space']['high']).all() and (values >= env_params['a_space']['low']).all(),\
         "Counterfactual out of action space"
 
+    # Translate queried timesteps to indices
     begin_index = int(np.round(t_begin / env_params['delta_t']))
     end_index = int(np.round(t_end / env_params['delta_t']))
     len_indices = end_index - begin_index + 1
     horizon += len_indices # Re-adjusting horizon
 
+    # Regenerate trajectory data with noise disabled
     env_params['noise'] = False  # For reproducibility
     env = make_env(env_params)
     figures = []
 
-    # Regenerating trajectory data with noise disabled
     evaluator, data = env.get_rollouts({'Actual': policy}, reps=1, get_Q=True)
 
-    # Action CF generations here
+    # Obtain counterfactual action trajectories
     orig_traj = data['Actual']['u'] # (action_dim, instances, n_reps=1)
     cf_traj = orig_traj.copy()
 
@@ -39,6 +51,7 @@ def cf_by_action(t_begin, t_end, actions, values, policy, horizon=10):
         v = values[i]
         cf_traj[action_index, begin_index:end_index + 1, :] = np.array([v for _ in range(len_indices)])[:,np.newaxis]
 
+    # Obtain rollout data from counterfactual action trajectories
     cf_settings = {
         'CF_mode': 'action',
         'begin_index': begin_index,
@@ -52,6 +65,7 @@ def cf_by_action(t_begin, t_end, actions, values, policy, horizon=10):
     evaluator.policies[f'CF: {label}'] = policy
     evaluator.data = data | cf_data
 
+    # Get rollout data results for actual & counterfactual trajectories
     interval = [begin_index-1, begin_index + horizon] # Interval to watch the control results
     fig = evaluator.plot_data(evaluator.data, interval=interval)
 
