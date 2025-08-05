74bca4ca078b1ae03db4b9eb971e6d626e45704a
esby <luka70@naver.com>
2025-08-04 20:53:54 -0400

    [MAJOR] base explainer done (25.08.04)

--- explainer/base_explainer.py
+++ explainer/base_explainer.py
@@ -5,13 +5,11 @@ current_dir = os.getcwd()
 figure_dir = os.path.join(current_dir, 'figures')
 os.makedirs(figure_dir, exist_ok=True)
 
-# %% SHAP module
+# %% Base explainer module
 class Base_explainer:
     def __init__(self, model, bg, feature_names, algo, env_params):
         """
-        Args:
-            model: [pd.DataFrame] Data to be interpreted
-            bg: Background data
+        Base explainer object
         """
         self.model = model
         self.feature_names = feature_names
@@ -26,19 +24,13 @@ class Base_explainer:
         self.bg = bg
         self.bg = self._scale_X(bg)
 
-        # if self.model == 'DDPG':
-        #     print(f"Interpreting actor network, which outputs deterministic output from observations")
-        # elif self.model == 'SAC':
-        #     print(f"Interpreting actor network, which outputs deterministic output from observations")
-        # else:
-        #     print(f"Interpreting value network, which outputs each Q value from observations")
-
     def explain(self, X):
         pass
 
     def plot(self, values, max_display=10):
         pass
 
+    # Scaling and descaling functions for explanations
     def _scale_X(self, X):
         low = self.o_space['low'][np.newaxis, :]
         high = self.o_space['high'][np.newaxis, :]
